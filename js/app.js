ac02176e9d5c9312e68c97e286146f8921f7877d
TerryGoh <52595052+TerryGoh@users.noreply.github.com>
2019-08-05 20:22:08 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,490 @@
+const TCArray = new Object();
+
+const TC_ImgLogo = new Object();
+
+TCArray['#A_H'] = [
+    {
+        TC: "ALJUNIED - HOUGANG TC",
+        address: "Blk 810 Hougang Central #02-214",
+        postal_code: "530810",
+        mapPosition: { lat: 1.370580, lng: 103.892159 },
+    },
+    {
+        TC: "ALJUNIED - HOUGANG TC",
+        address: "Blk 701 Hougang Avenue 2",
+        postal_code: "530701",
+        mapPosition: { lat: 1.368390, lng: 103.883830 },
+    },
+
+    {
+        TC: "ALJUNIED - HOUGANG TC",
+        address: "Blk 216 Serangoon Avenue 4",
+        postal_code: "460549",
+        mapPosition: { lat: 1.357570, lng: 103.871500 },
+    },
+
+    {
+        TC: "ALJUNIED - HOUGANG TC",
+        address: "Blk 216 Serangoon Avenue 4",
+        postal_code: "550216",
+        mapPosition: { lat: 1.357570, lng: 103.871500 },
+    },
+];
+
+TCArray['#AMK'] = [
+    {
+        TC: "AMK TC",
+        address: "Blk 342 Ang Mo Kio Ave 1 #01-1561",
+        postal_code: "560342",
+        mapPosition: { lat: 1.364720, lng: 103.840973 },
+    },
+    {
+        TC: "AMK TC",
+        address: "Blk 528 Ang Mo Kio Ave 10 #01-2385",
+        postal_code: "560528",
+        mapPosition: { lat: 1.367190, lng: 103.855870 },
+    },
+
+    {
+        TC: "AMK TC",
+        address: "Blk 410 Fernvale Road (Multi-Storey Carpark)",
+        postal_code: "790410",
+        mapPosition: { lat: 1.371370, lng: 103.888010 },
+    },
+
+    {
+        TC: "AMK TC",
+        address: "Blk 662 Hougang Ave 4 #01-415",
+        postal_code: "530662",
+        mapPosition: { lat: 1.357570, lng: 103.871500 },
+    },
+];
+
+TCArray['#BT'] = [
+    {
+        TC: "BISHAN - TOA PAYOH TC",
+        address: "BLK 125A Lor 2 Toa Payoh #01-134",
+        postal_code: "311125",
+        mapPosition: { lat: 1.335550, lng: 103.845940 },
+    },
+    {
+        TC: "BISHAN - TOA PAYOH TC",
+        address: "BLK 197, Bishan St 13 #01-585",
+        postal_code: "570197",
+        mapPosition: { lat: 1.348340, lng: 103.853200 },
+    }
+];
+
+TCArray['#CCK'] = [
+    {
+        TC: "CCK TC",
+        address: "Blk 309 Choa Chu Kang Ave 4 #02-02 Choa Chu Kang Centre",
+        postal_code: "560342",
+        mapPosition: { lat: 1.364720, lng: 103.840973 },
+    },
+    {
+        TC: "CCK TC",
+        address: "Blk 528 Ang Mo Kio Ave 10 #01-2385",
+        postal_code: "680309",
+        mapPosition: { lat: 1.393820, lng: 103.746094 },
+    },
+
+    {
+        TC: "CCK TC",
+        address: "Blk 524 Bukit Batok St 52 #01-769",
+        postal_code: "650524",
+        mapPosition: { lat: 1.354300, lng: 103.752480 },
+    },
+
+    {
+        TC: "CCK TC",
+        address: "Blk 952 Jurong West St 91 #01-725",
+        postal_code: "640952",
+        mapPosition: { lat: 1.341210, lng: 103.688440 },
+    },
+];
+
+TCArray['#EC_FS'] = [
+    {
+        TC: "EAST COAST-FENGSHAN TC",
+        address: "Blk 206 Bedok North St 1 #01-353",
+        postal_code: "460206",
+        mapPosition: { lat: 1.326150, lng: 103.930740 },
+    },
+];
+
+TCArray['#HL_BP'] = [
+    {
+        TC: "HOLLAND - BUKIT PANJANG TC",
+        address: "Block 256 Bangkit Road #01-71",
+        postal_code: "670256",
+        mapPosition: { lat: 1.378880, lng: 103.773860 },
+    },
+    {
+        TC: "HOLLAND - BUKIT PANJANG TC",
+        address: "170 Ghim Moh Rd #06-02 Ulu Pandan Community Building",
+        postal_code: "279621",
+        mapPosition: { lat: 1.310390, lng: 103.788177 },
+    }
+];
+
+TCArray['#JB'] = [
+    {
+        TC: "JALAN BESAR TC",
+        address: "Block 70 Geylang Bahru #01-2719",
+        postal_code: "330070",
+        mapPosition: { lat: 1.321780, lng: 103.870911 },
+    },
+    {
+        TC: "JALAN BESAR TC",
+        address: "Block 121 Potong Pasir Avenue 1 #01-271",
+        postal_code: "350121",
+        mapPosition: { lat: 1.334790    , lng: 103.866100 },
+    }
+];
+
+TCArray['#JR'] = [
+    {
+        TC: "JURONG - CLEMENTI TC",
+        address: "Blk 255 Jurong East Street 24 #01-303",
+        postal_code: "600255",
+        mapPosition: { lat: 1.342890, lng: 103.740330 },
+    },
+    {
+        TC: "JURONG - CLEMENTI TC",
+        address: "Blk 630 Bukit Batok Central #01-146",
+        postal_code: "650630",
+        mapPosition: { lat: 1.349820, lng: 103.749863 },
+    },
+
+    {
+        TC: "JURONG - CLEMENTI TC",
+        address: "Blk 399 Yung Sheng Road #01-20 Taman Jurong Shopping Centre",
+        postal_code: "610399",
+        mapPosition: { lat: 1.334180, lng: 103.721630},
+    },
+
+    {
+        TC: "JURONG - CLEMENTI TC",
+        address: "Blk 509 Jurong West Street 51 #01-159",
+        postal_code: "640509",
+        mapPosition: { lat: 1.347830, lng: 103.720497 },
+    },
+
+    {
+        TC: "JURONG - CLEMENTI TC",
+        address: "Blk 324 Clementi Avenue 5 (Void Deck)",
+        postal_code: "550216",
+        mapPosition: { lat: 1.315160, lng: 103.766040},
+    },
+];
+
+TCArray['#MP'] = [
+    {
+        TC: "MARINE PARADE TC",
+        address: "Blk 50 Marine Terrace #01-265",
+        postal_code: "440050",
+        mapPosition: { lat: 1.305330, lng: 103.916120 },
+    },
+    {
+        TC: "MARINE PARADE TC",
+        address: "Blk 266 Serangoon Central Drive #03-251",
+        postal_code: "550266",
+        mapPosition: { lat:1.353430, lng: 103.872040 },
+    }
+];
+
+
+TCArray['#MS'] = [
+    {
+        TC: "MARSILING-YEW TEE TC",
+        address: "Blk 306A Woodlands Street 31, #02-00,",
+        postal_code: "731306",
+        mapPosition: { lat: 1.430520, lng: 103.775370 },
+    },
+    {
+        TC: "MARSILING-YEW TEE TC",
+        address: "Blk 309 Choa Chu Kang Avenue 4 #02-02 Choa Chu Kang Centre",
+        postal_code: "680309",
+        mapPosition: { lat: 1.393820, lng: 103.746090 },
+    },
+    {
+        TC: "MARSILING-YEW TEE TC",
+        address: "Blk 900 South Woodlands Drive #02-02",
+        postal_code: "730900",
+        mapPosition: { lat: 1.435480, lng: 103.787529 },
+    }
+];
+
+TCArray['#NS'] = [
+    {
+        TC: "NEE SOON TC",
+        address: "Block 290 Yishun Street 22",
+        postal_code: "760290",
+        mapPosition: { lat: 760290, lng: 103.838290 },
+    },
+    {
+        TC: "NEE SOON TC",
+        address: "Block 751 Yishun Street 72 #01-186",
+        postal_code: "760751",
+        mapPosition: { lat: 1.426740, lng: 103.833200 },
+    },
+
+    {
+        TC: "NEE SOON TC",
+        address: "Block 845 Yishun Street 81 #02-00",
+        postal_code: "760845",
+        mapPosition: { lat: 1.414220, lng: 103.835442 },
+    },
+
+    {
+        TC: "NEE SOON TC",
+        address: "Block 161 Ang Mo Kio Ave 4 #01-500",
+        postal_code: "560161",
+        mapPosition: { lat: 1.377610, lng: 103.838058 },
+    },
+];
+
+
+
+TCArray['#PP'] = [
+    {
+        TC: "PASIR RIS - PUNGGOL TC",
+        address: "Blk 577 Pasir Ris St 53 #01-88",
+        postal_code: "510577",
+        mapPosition: { lat: 1.374310, lng: 103.946750 },
+    },
+    {
+        TC: "PASIR RIS - PUNGGOL TC",
+        address: "Blk 603 Punggol Road #01-01",
+        postal_code: "820603",
+        mapPosition: { lat: 1.407120, lng:103.908490 },
+    },
+
+    {
+        TC: "PASIR RIS - PUNGGOL TC",
+        address: "Blk 266 Compassvale Bow #01-06",
+        postal_code: "540266",
+        mapPosition: { lat: 1.381490, lng: 103.893890},
+    },
+
+    {
+        TC: "PASIR RIS - PUNGGOL TC",
+        address: "Blk 156A Rivervale Crescent #01-01",
+        postal_code: "541156",
+        mapPosition: { lat: 1.390510, lng: 103.907210 },
+    },
+];
+
+
+TCArray['#SW'] = [
+    {
+        TC: "SEMBAWANG TC",
+        address: "Blk 504C Canberra Link #01-63",
+        postal_code: "753504",
+        mapPosition: { lat: 1.447330, lng: 103.827050 },
+    },
+    {
+        TC: "SEMBAWANG TC",
+        address: "Blk 900 South Woodlands Drive #02-02",
+        postal_code: "730900",
+        mapPosition: { lat: 1.435480, lng:103.787529 },
+    }
+];
+
+TCArray['#TP'] = [
+    {
+        TC: "TAMPINES TC",
+        address: "136 Tampines Street 11",
+        postal_code: "521136",
+        mapPosition: { lat: 1.346700, lng: 103.947609 },
+    },
+] ;
+
+TCArray['#TJP'] = [
+    {
+        TC: "TANJONG PAGAR TC",
+        address: "Block 166 Bukit Merah Central #03-3527",
+        postal_code: "150166",
+        mapPosition: { lat: 1.283340, lng: 103.816490 },
+    },
+] ;
+
+
+TCArray['#WC'] = [
+    {
+        TC: "WEST COAST TC",
+        address: "Block 441A Clementi Avenue 3 #02-01",
+        postal_code: "121441",
+        mapPosition: { lat: 1.314390, lng: 103.764420},
+    },
+    {
+        TC: "WEST COAST TC",
+        address: "Block 509 Jurong West Street 51 #01-159",
+        postal_code: "640509",
+        mapPosition: { lat: 1.347830, lng: 103.720500 },
+    },
+
+    {
+        TC: "WEST COAST TC",
+        address: "Block 79/82 Telok Blangah Drive (Market & Food Centre)",
+        postal_code: "540266",
+        mapPosition: { lat: 1.273380, lng: 103.807700 },
+    },
+
+    {
+        TC: "WEST COAST TC",
+        address: "Tanjong Pagar Town Council Block 166 Bukit Merah Central #03-3527",
+        postal_code: "150166",
+        mapPosition: { lat: 1.283340, lng: 103.816490 },
+    },
+];
+
+
+const TCMarkerArray = new Object;
+
+var map;
+
+var createMap = (mapPosition) => {
+    console.log("create Map here")
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: mapPosition,
+        zoom: 12
+    });
+}
+
+
+
+var loadMarkerIcon = (imgUrl) => {
+
+    var icon = {
+        url: imgUrl, // url
+        scaledSize: new google.maps.Size(60, 50), // size
+    };
+    return icon;
+}
+
+var createTCMarkers = (id) => {
+    var marker;
+    var tc;
+    var tcArray_id = TCArray[id];
+    console.log(id);
+    console.log(TCArray['#A_H'])
+    TCMarkerArray[id] = [];
+
+    console.log(TC_ImgLogo)
+
+
+    for (let i = 0; i < tcArray_id.length; i++) {
+        tc = tcArray_id[i];
+
+        console.log(tc.mapPosition);
+        marker = new google.maps.Marker({
+            position: tc.mapPosition,
+            //      map: map,
+            title: tc.TC + tc.address,
+            //      icon: loadMarkerIcon(tc.imgUrl),
+            icon: loadMarkerIcon(TC_ImgLogo[id]),
+
+
+        });
+        TCMarkerArray[id].push(marker);
+
+    }
+}
+
+var setMarkersVisible = (id, map1) => {
+
+    let tcMarkerArray = TCMarkerArray[id];
+
+    if (tcMarkerArray == null || tcMarkerArray.length == 0)
+        return;
+
+    console.log("tcMarkerArray.length == " + tcMarkerArray.length);
+    for (let i = 0; i < tcMarkerArray.length; i++) {
+        tcMarkerArray[i].setMap(map1);
+    }
+}
+
+function initMap() {
+    var mapPosition;
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        var mapPosition = { lat: 1.3098822, lng: 103.7753119 };
+
+        console.log("mapPosition == " + mapPosition);
+
+        var map = createMap(mapPosition);
+
+    });
+
+}
+
+
+
+var initAllImgCB = () => {
+    var elems = document.querySelectorAll('div');
+    var imgArray = elems[0].querySelectorAll('img');
+
+
+    for (let i = 0; i < imgArray.length; i++) {
+        console.log(imgArray[i].id);
+        console.log(imgArray[i].src);
+
+
+        let imgId = `#${imgArray[i].id}`;
+        let visible = true;
+        if (!TC_ImgLogo[imgId])
+            TC_ImgLogo[imgId] = imgArray[i].src;
+
+        document.querySelector(imgId).addEventListener('click',
+            function () {
+                console.log(visible);
+                console.log(imgId);
+
+                if (!TCMarkerArray[imgId] || TCMarkerArray[imgId].length === 0)
+                    createTCMarkers(imgId);
+
+
+                if (visible) {
+                    setMarkersVisible(imgId, map);
+                    visible = false;
+                }
+                else {
+                    setMarkersVisible(imgId, null);
+                    visible = true;
+                }
+
+            }, false);
+
+    }
+}
+
+var initServiceWorkerForClient = () => {
+    if (!('serviceWorker' in navigator)) {
+        console.log('Service Worker not supported');
+        return;
+    }
+
+    navigator.serviceWorker.register('service-worker.js')
+        .then(() => {
+            console.log('Registered Service Worker');
+            //subscribePushNotification(); //taken away in ex. 3 to subscribe on click of button
+        })
+        .catch(error => {
+            console.log("Registering of Service Worker Failed: " + error);
+        });
+
+}
+
+window.onload = function () {
+
+    
+    initMap();
+
+    initAllImgCB();
+    
+    initServiceWorkerForClient() ;
+    
+} 
